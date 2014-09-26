99e5294c38e4e104ac837e6d884ce0b36af89032
Peter Shaw <unthoughted@googlemail.com>
2014-09-26 13:12:06 +0200

    plausieble werte
    nur in vollen grad schritten, siehe: http://www.mikrocontroller.net/topic/106021

--- Firmware/src/sensor/max6675.c
+++ Firmware/src/sensor/max6675.c
@@ -7,7 +7,7 @@
 ISR (TIMER0_OVF_vect) {
     static count = 0;
     count++;
-    if(count == interval){
+    if(count >= interval){
         count = 0;
         // read tmp
         shouldReadTemp = 1;
@@ -16,36 +16,38 @@ ISR (TIMER0_OVF_vect) {
 
  uint16_t gettemp(){
      uint16_t act_temp;
-     uint8_t msb;
-     uint8_t lsb;
      act_temp = 0;
     
       uint8_t   bytecounter = 0;
-      uint8_t Bitnr = 7;
+      uint8_t Bitnr = 0;
       PORTB &= ~(1 << PB0);                                             
       _delay_us(100);
-      for(bytecounter = 0 ; bytecounter < 8 ; bytecounter++){           
-          PORTB |= (1 << PB5);                                          
-          msb += (PINB & (1 << PB4)) *2 ^Bitnr--;
-          PORTB &= ~(1 << PB5);                                 
-      }
-      
-      Bitnr = 7;
-      for(bytecounter = 0 ; bytecounter < 8 ; bytecounter++){
-          PORTB |= (1 << PB5);                              
-          lsb += (PINB & (1 << PB4)) *2 ^Bitnr--;
-          PORTB &= ~(1 << PB5);                 
-      }
-      PORTB |= (1 << PB0);                      
-      
-      
-    lsb =   lsb >> 3;
-//  msb =   msb << 1;
-//  msb =   msb >> 5;
-//      act_temp  = msb << 8;
-      act_temp  = lsb;
-      
-      act_temp = act_temp * 0.25;
+
+      // for(bytecounter = 0 ; bytecounter < 16 ; bytecounter++){           
+//           PORTB |= (1 << PB5);       
+//           if(bytecounter >= 3 && bytecounter <= 14 ){                                   
+//           	act_temp += (PINB & (1 << PB4)) *2 ^ Bitnr++;
+//           }
+//           PORTB &= ~(1 << PB5);                                 
+//       }
+  for(int i=0; i<10; i++){
+
+    //clk-toggle for next bit (skips irrelevant msb in first iteration)
+    PORTB |= (1 << PB5);
+    _delay_ms(1);
+    PORTB &= ~(1 << PB5);
+    _delay_ms(1);
+
+    // shift
+    act_temp = act_temp << 1;
+    
+    // add pin state (0 or 1)
+    act_temp += ((PINB >> PB4) & 1);       
+  }
+
+       PORTB |= (1 << PB0);                      
+    
+   //   act_temp = act_temp * 0.25;
       
     return act_temp;
  }
@@ -57,9 +59,9 @@ uint8_t getTC(void){
     for(bytecounter = 0 ; bytecounter < 16 ; bytecounter++){
         PORTB |= (1 << PB5);                            
         if(bytecounter == 2){                           
-            if((PINB & (1 << PB4))){                    
+            if((PINB & (1 << PB4))){          
                 TC = 0;
-            }else{                                      
+            } else {                                      
                 TC = 255;
             }
         }
@@ -72,16 +74,21 @@ uint8_t getTC(void){
 void init_max6675(){
     shouldReadTemp = 0;
 
+	//clk and cs as output
     DDRB |=  (1 << PB5) | (1 << PB0);   
     DDRB &= ~(1 << PB4);
+        
+	// pullup
+	PORTB |= (1<<PB4); 
     
-    interval = 20;
+    //cs high for measure
+	DDRB |= (1<<PB0);
+    
     max6675_error = 0;
-    interval = getTC();
-     if(interval == 255){
+     if(getTC() == 255){
          interval = 20;
      } else {
-         interval = 8;
+         interval = 1;
          max6675_error = 1;
      }
     // Enable timer
